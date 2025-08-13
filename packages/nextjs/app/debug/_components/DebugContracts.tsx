3cd27b3fdf0797b3036bb8c33a8d8284f840db4a
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-08-14 00:35:56 +0530

    Smart Cached to optimize

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -32,7 +32,7 @@ export default function DebugContracts() {
     try {
       if (typeof window === "undefined") return;
 
-      const contractAddress = "0x85bd1a76e588e2539fd450afd6255806e7026049";
+      const contractAddress="0xa92156fd3a8573b5c4fe4bdce567ddf5a2f5e864";
       const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL || '';
       const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || '';
       // const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
